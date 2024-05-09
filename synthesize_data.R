2eb081c168d55d5a9ff94333c69d1fec6d8d5ad2
Noah Bierwirth <noahbierwirth@Noahs-MacBook-Air-2.local>
2024-05-09 13:10:33 -0500

    creating example_data.csv

--- synthesize_data.R
+++ synthesize_data.R
@@ -3,10 +3,10 @@ library(rio)
 library(dplyr)
 library(ggplot2)
 
-hyperglycemia_bpd <- import("~/Desktop/Research/Hyperglycemia:BPD Paper/demo.df.csv")
+hyperglycemia_bpd <- import("example_data.csv")
 
-hyperglycemia_bpd <- import("~/Desktop/numerlical_glucose_df.csv")[
-  ,c("ga", "bw", "gender", "vent_days", "bpd_or_death", "average72hr", "top_third")]
+# hyperglycemia_bpd <- import("~/Desktop/numerlical_glucose_df.csv")[
+#  ,c("ga", "bw", "gender", "vent_days", "bpd_or_death", "average72hr", "top_third")]
 
 data_syn <- syn(hyperglycemia_bpd)
 
